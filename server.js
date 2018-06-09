1dd1932d3e3edc9fb53b17d06ec9af9b723a2571
Kevin Galvan <klgalvan79@gmail.com>
2018-06-08 19:25:07 -0500

    second

--- server.js
+++ server.js
@@ -4,22 +4,8 @@ const PORT = process.env.PORT || 3334;
 
 http.Server((req, res) => {
 
-    switch (req.url) {
-        case '/':
-            fs.readFile('./index.html', 'utf8', (err, data) => {
-                if (err) {
-                    res.end(err)
-                } else {
-                    res.end(data)
-                }
-            })
-                break;
-        case '/favicon':
-            res.end('my favorite')
-                break;
-        default:
-            res.end('what in fucks cunt are you doing?')
-                break;
+    if (req.url === '/') {
+        res.send()
     }
 
 }).listen(PORT, () => console.log('localhost:'+ PORT))
