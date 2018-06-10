accc26b6413ef4431ddcdf70a5e6cf4a0eb341f8
Kevin Galvan <klgalvan79@gmail.com>
2018-06-10 01:32:41 -0500

    neat

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3334;
+const serveStatic = require('serve-static');
+const path = require('path');
+
+
+app.use(serveStatic(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('dome sweet dome')
+    res.send();
 })
 app.get('/favicon.ico', (req, res) => {
     res.send('my favorite')
 })
 app.listen(PORT, () => console.log('localhost:'+ PORT))
+
+
