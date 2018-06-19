23c749ad45e27f5a17bfd55bec18dc4c05010035
Kevin Galvan <klgalvan79@gmail.com>
2018-06-19 18:01:50 -0500

    interface

--- sur.js
+++ sur.js
@@ -3,13 +3,28 @@ const app = express();
 const PORT = process.env.PORT || 3334;
 const serveStatic = require('serve-static');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 
 
 app.use(serveStatic(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+
 app.get('/keyboard', (req, res) => {
     res.sendFile(path.join(__dirname + '/keyboard.html'));
 })
+
+app.get('/newStuff', (req, res) => {
+    MongoClient.connect("mongodb://localhost:27017/music", function(err, client) {
+    let db = client.db('music');
+        db.collection('notes').find().toArray(function(err, result) {
+            res.render('keyboard', {
+                result:result
+            });
+        });
+    });
+})
+
 
 app.get('/favicon.ico', (req, res) => {
     res.send('my favorite')
